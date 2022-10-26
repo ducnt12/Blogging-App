4d158fa5c064548142835b50f5b90cf33a203409
ducnt12 <hello.there.two.nine@gmail.com>
2022-10-26 12:36:44 +0700

    update get and post method

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Blogging-App
+# Blogging App
+Server-side redering blogging app.
+# Installation
+## DB Initilization (only needed with local host db)
+1. Use `from file_name import app,db` to import app and db variables
+2. `with app.app_context():`
+3. \t`db.create_all()`
+4. Enter again to exit the python function
+# Tech Stack
+- Backend: Flask 
--- app.py
+++ app.py
@@ -1,26 +1,40 @@
-from flask import Flask, render_template
+from datetime import datetime
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy 
+
+# use uuid to generate unique id in python
+# To send api, use jsonify from flask
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
 
-all_posts = [
-    {
-        "title": "Post 1",
-        "content": "This is the content of post 1. Lalalalala",
-        "author": "Aron"
-    },
-    {
-        "title": "Post 2",
-        "content": "This is the content of post 2. Lalalalala"
-    }
-]
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=True)
+    author = db.Column(db.String(20), nullable=False, default="N/A")
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return 'Blog post ' + str(self.title) + '\nBy: ' + str(self.id)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/posts")
+@app.route("/posts", methods=["GET", "POST"])
 def posts():
-    return render_template("posts.html", posts=all_posts)
+    if request.method == "POST":
+        post= request.form
+        post = BlogPost(title=post['title'], content=post['content'], author=post['author'])
+        db.session.add(post)
+        db.session.commit()
+
+        return redirect('/posts')
+    else:
+        all_posts = BlogPost.query.order_by(BlogPost.date).all()
+        return render_template("posts.html", posts=all_posts)
 
 @app.route("/users/<string:name>")
 def hello(name):
